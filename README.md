1ba64a50037e4adaa3084868c2e7f108b17f447a
Alexandra Hionis <Alexandra.Hionis@gmail.com>
2021-06-08 20:41:13 -0400

    Started Readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Tip-Calculator
+
+https://alexandra-hionis.github.io/Tip-Calculator/
+
+## Description
+
+Let's say you had a great dinner and want to calculate the tip quickly and efficiently. The user can enter the bill amount and select average, most commonly used tip percentages 15%, 20%, and 25%. User can also enter a custom tip if they decide to. Results will show the bill total and the tip amount.
+
+## Table of Contents
+
+- [Description](#description)
+- [Usage](#usage)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Usage
+
+A simple application where all the user needs to do is enter inputs or press a button and their tip and bill total are magically displayed.
+
+![image 1](assets/images/sample-1.png)
+![image 2](assets/images/sample-2.png)
+![video](https://media.giphy.com/media/eaqtFnAFebguCbpscK/giphy.gif)
+
+https://media.giphy.com/media/eaqtFnAFebguCbpscK/source.mov
+
+## License
+
+ISC, MIT
+
+## Tests
+
+No tests were used
+
+## Questions
+
+GitHub: https://github.com/Alexandra-Hionis<br /><br />
+Email: Alexandra.hionis@gmail.com<br /><br />
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Hide custom tip section
 document.getElementById("customTipSection").style.display = "none";
 
 function calculateTip() {
@@ -12,10 +13,12 @@ function calculateTip() {
       let userSelection;
       for (const selection of selections) {
         if (selection.checked) {
+          // Get value of selected radio button
           userSelection = selection.value;
           break;
         }
       }
+      // Use toFixed to make sure that the number has two digits ater the decimal
       const tipAmount = userSelection * billAmount;
       document.getElementById("tipAmount").innerHTML =
         "$" + tipAmount.toFixed(2);
@@ -24,11 +27,6 @@ function calculateTip() {
         "$" + totalAmount.toFixed(2);
     };
   }
-}
-
-function showDiv() {
-  document.getElementById("radioTipSection").style.display = "none";
-  document.getElementById("customTipSection").style.display = "block";
 }
 
 function customTip() {
@@ -40,22 +38,19 @@ function customTip() {
   if (billAmountCustomTip.length == "" || customPercentInput == "") {
     myModal.style.display = "block";
   } else {
-    // const calculateBtn = document.querySelector("#calculateBtn");
-    // calculateBtn.onclick = function () {
-    //   const selections = document.querySelectorAll('input[name="tip"]');
-    //   let userSelection;
-    //   for (const selection of selections) {
-    //     if (selection.checked) {
-    //       userSelection = selection.value;
-    //       break;
-    //     }
-    //   }
+    // Divide by 100 to ensure correct %
     const tipAmount = (customPercentInput / 100) * billAmountCustomTip;
+    // Use toFixed to make sure that the number has two digits ater the decimal
     document.getElementById("tipAmount").innerHTML = "$" + tipAmount.toFixed(2);
     const totalAmount = parseInt(billAmountCustomTip) + tipAmount;
     document.getElementById("totalAmount").innerHTML =
       "$" + totalAmount.toFixed(2);
   }
+}
+
+function showDiv() {
+  document.getElementById("radioTipSection").style.display = "none";
+  document.getElementById("customTipSection").style.display = "block";
 }
 
 // Refresh Page
